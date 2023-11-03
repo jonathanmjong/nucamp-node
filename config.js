dd54356f3bcd0f43cc3e45eba8b25acc21d0b1d9
Jonathan Jong <jonathanmjong@gmail.com>
2023-11-03 12:36:03 -0500

    config

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 const dotenv = require("dotenv").config();
 
 module.exports = {
-  secretKey: "867-5309867-5310867-5311867-5313",
-  mongoUrl:
-    "mongodb+srv://jjnucampoctober2023:Bc62mY2X7Nu8UDC8@cluster0.gs0pa8n.mongodb.net/nucamp?retryWrites=true&w=majority",
+  secretKey: process.env.PASSPORTJWTKEY,
+  mongoUrl: process.env.MONGOURI,
 };
