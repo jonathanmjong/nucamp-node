9d22b5cd20b6f396d143f16e297552aa0ee316b7
Jonathan Jong <jonathanmjong@gmail.com>
2023-11-03 12:29:34 -0500

    workshop3

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
-const dotenv = require('dotenv').config();
+const dotenv = require("dotenv").config();
 
 module.exports = {
-    'secretKey': '867-5309867-5310867-5311867-5313',
-    'mongoUrl': process.env.SECRET_URL
-}
+  secretKey: "867-5309867-5310867-5311867-5313",
+  mongoUrl:
+    "mongodb+srv://jjnucampoctober2023:Bc62mY2X7Nu8UDC8@cluster0.gs0pa8n.mongodb.net/nucamp?retryWrites=true&w=majority",
+};
